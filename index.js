d4a9423cceb8cf2cf12684ba9f4d93e9ef8181d2
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-05-17 22:52:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+  
+// Your code here
+function mapToNegativize(sourceArray){
+  let new_array = []
+  for (let element of sourceArray){
+    let e = element * -1
+    new_array.push(e)
+  }
+  return new_array
+}
+
+function mapToNoChange(sourceArray){
+  return sourceArray
+}
+
+function mapToDouble(sourceArray){
+  let new_array = []
+  for (let element of sourceArray){
+    new_array.push(element * 2)}
+    return new_array
+}
+
+function mapToSquare(sourceArray){
+  let new_array = []
+  for (let element of sourceArray){
+  new_array.push(element * element)}
+  return new_array
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+  let total = startingPoint
+  for (let element of sourceArray){
+    total += element
+  }
+  return total
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+}
